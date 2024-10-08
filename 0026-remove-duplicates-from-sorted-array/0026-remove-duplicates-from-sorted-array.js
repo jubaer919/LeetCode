58237d603dc7282bf1f1195e4a95fcd3f60c939f
Jubaer_Maruf <jubaer.diu@gmail.com>
2024-10-08 10:30:50 +0600

    Time: 69 ms (37.57%), Space: 52.7 MB (31.65%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+   let i = 1
+   let j = 1
+   
+   while (j < nums.length) {
+       if (nums[i - 1] !== nums[j]) {
+           nums[i] = nums[j]
+           i += 1
+       }
+       j += 1
+   }
+    return i
+};
