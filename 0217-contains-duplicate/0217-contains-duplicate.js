7302e040de960ba8c01faea70366b774df4bef27
Jubaer_Maruf <jubaer.diu@gmail.com>
2024-10-09 12:08:37 +0600

    Time: 69 ms (90.69%), Space: 61.1 MB (66.17%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -4,13 +4,12 @@
  */
 var containsDuplicate = function(nums) {
     let hashmap = {}
-    
-    for(let num of nums) {
-        if (num in hashmap) {
+    for (let num of nums) {
+        if(hashmap[num]) {
             return true
         }
+        
         hashmap[num] = (hashmap[num] || 0) + 1
     }
-    
     return false
 };
